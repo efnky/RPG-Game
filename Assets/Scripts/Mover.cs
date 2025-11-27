38027b4e20e39617481e5fb18e2aa34d6b80889d
efnky <efeinkaya@gmail.com>
2025-11-27 22:31:24 +0100

    :sparkles: Basic character mouvements implemented.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,13 +1,46 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButton(1))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
     }
 }
